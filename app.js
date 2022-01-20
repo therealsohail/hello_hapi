2f8b8bf04c379c84aa586c89bbcfd936449caf54
Muhammad Sohail <50323869+therealsohail@users.noreply.github.com>
2022-01-20 18:03:36 +0500

    test pipeline

--- app.js
+++ app.js
@@ -1,35 +1,32 @@
-'use strict';
+"use strict";
 
-const Hapi   = require('@hapi/hapi');
+const Hapi = require("@hapi/hapi");
 
 const Server = new Hapi.Server({
-    host: 'localhost',
-    port: 3000
+  host: "localhost",
+  port: 3000,
 });
-const Hello  = require('./lib/hello');
+const Hello = require("./lib/hello");
 
 Server.route({
-    method: 'GET',
-    path: '/hello/{user}',
-    handler: function (request, reply) {
-
-        const result = Hello(decodeURIComponent(request.params.user));
-        return result;
-    }
+  method: "GET",
+  path: "/hello/{user}",
+  handler: function (request, reply) {
+    const result = Hello(decodeURIComponent(request.params.user));
+    return result;
+  },
 });
 
 // don't start server if this file was required
 
 if (!module.parent) {
+  Server.start((err) => {
+    if (err) {
+      throw err;
+    }
 
-    Server.start((err) => {
-
-        if (err) {
-            throw err;
-        }
-
-        console.log(`Server running at: ${Server.info.uri}`);
-    });
+    console.log(`Server running at: ${Server.info.uri}`);
+  });
 }
 
 module.exports = Server;
